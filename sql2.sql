5f0944fcd3a51875731333c5ac3ed3677423a4d0
nainatrigvent <naina@trigvent.com>
2022-04-13 16:10:08 +0530

    latestchanges

--- sql2.sql
+++ sql2.sql
@@ -1,9 +1,13 @@
 create database univ;
 use univ;
+drop table lib;
 create table lib (
-	isbn bigint primary key,
+	isbn bigint primary key auto_increment,
 	author varchar(20),
-	publication datetime,
+	publication varchar(50),
 	book_title varchar(70)
 );
+desc lib;
+insert lib (author, publication,book_title) values( "reema", "03-06-1996", "the world web");
+select * from lib where isbn>2;
 
